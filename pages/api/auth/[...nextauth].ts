69cb9d55154845206055be6aab2619bd3cb27937
Amir Samoha <amir.samoha42@gmail.com>
2023-07-16 18:46:22 +0300

    add stripe-and make new user in stripe

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,14 +2,34 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client"
+import Stripe from "stripe";
 
 const prisma = new PrismaClient()
 export default NextAuth({
-    adapter: PrismaAdapter(prisma) as any,
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      }), 
-    ],
-  });
+  adapter: PrismaAdapter(prisma) as any,
+  providers: [
+    GoogleProvider({ // sidn in with google account
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  events: { // create new user in stripe with google account that signed in
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: '2022-11-15',
+      })
+      //create stripe customer
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name
+        })
+        //also update the prisma user with with the stripe customerId
+        await prisma.user.update({
+          where: { id: user.id }, // find the prisma user that signed 
+          data: { stripeCustomerId: customer.id }, // update the customerId with the new user in stripe
+        })
+      }
+    },
+  },
+});
